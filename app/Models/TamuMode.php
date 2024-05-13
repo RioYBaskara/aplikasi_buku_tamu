24a8a5b411e6d9be3ef99195ebe872a612b1ebc0
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-05-14 02:36:38 +0700

    New | Model, TamuMode

---
+++ app/Models/TamuMode.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class tamuModel extends Model{
+    protected $table = 'tb_tamu';
+    protected $primaryKey = 'id';
+    protected $returnType = 'object';
+    protected $allowedFields = ['tgl_berkunjung', 'nama_tamu', 'alamat', 'no_telepon', 'keperluan'];
+}
